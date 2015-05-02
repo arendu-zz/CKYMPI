70c14da775ce305ca749bdb8ad81ddaaf0906ae5
arendu <adithya.r@gmail.com>
2015-05-02 16:12:05 -0400

    more buggy code

--- src/mylife.cpp
+++ src/mylife.cpp
@@ -60,6 +60,12 @@ int main(int argc, char **argv) {
     }
     vector<Cell> stack = process2stack[myProcessID];
 
+    for (auto kv: cell2process) {
+        Cell c = kv.first;
+        int processes = kv.second;
+        printf("cell (%d,%d) is mapped to process %d\n", c.first, c.second, processes);
+
+    }
 
     printf("initial stack: %d\n", myProcessID);
     for (Cell cell : stack) {
@@ -74,19 +80,23 @@ int main(int argc, char **argv) {
                 int from_processID = cell2process[make_pair(cell.first, j)];
                 MPI_Recv(recv, 2, MPI_INT, from_processID, TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
                 Cell reCell = make_pair(recv[0], recv[1]);
+                printf("process %d cell (%d,%d) received from process %d cell (%d,%d)\n", myProcessID, cell.first, cell.second, from_processID, recv[0],
+                       recv[1]);
                 recievedCells.push_back(reCell);
             }
             for (int i = cell.second; i > cell.first; i--) {
                 //reveice from (i, cell.second)
                 int recv[2] = {-1};
-                int from_processID = cell2process[make_pair(cell.first, j)];
+                int from_processID = cell2process[make_pair(i, cell.second)];
                 MPI_Recv(recv, 2, MPI_INT, from_processID, TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
                 Cell reCell = make_pair(recv[0], recv[1]);
+                printf("process %d cell (%d,%d) received from process %d cell (%d,%d)\n", myProcessID, cell.first, cell.second, from_processID, recv[0],
+                       recv[1]);
                 recievedCells.push_back(reCell);
             }
-            for (Cell reCell : recievedCells) {
-                printf("process %d, received  content (%d,%d)\n", myProcessID, reCell.first, reCell.first);
-            }
+//            for (Cell reCell : recievedCells) {
+            //printf("process %d, received  content (%d,%d)\n", myProcessID, reCell.first, reCell.first);
+//            }
         }
 
         // now compute the sends
@@ -95,34 +105,41 @@ int main(int argc, char **argv) {
             int message[2];
             message[0] = cell.first;
             message[1] = cell.second;
-            for (int i = cell.second + 1; i < N - 1; i++) {
+            int mid = 0;
+            for (int i = cell.second + 1; i < N; i++) {
                 //send to (cell.first, i)
                 int to_processID = cell2process[make_pair(cell.first, i)];
-                printf("process %d sending msg to %d, content (%d,%d)\n", myProcessID, to_processID, cell.first, cell.second);
+                printf("process %d sending (%d,%d)  to process %d (%d,%d)\n", myProcessID, cell.first, cell.second, to_processID, cell.first, i);
                 MPI_Send(&message, 2, MPI_INT, to_processID, TAG, MPI_COMM_WORLD);
+                //string msg;
+                //msg = to_string(cell.first) + "," + to_string(i), " msgid:" + to_string(mid);
+                //sendMessage(msg, to_processID, TAG);
             }
             for (int i = cell.first - 1; i >= 0; i--) {
                 // send to (i, cell.second)
                 int to_processID = cell2process[make_pair(i, cell.second)];
-                printf("process %d sending msg to %d, content (%d,%d)\n", myProcessID, to_processID, cell.first, cell.second);
+                printf("process %d sending (%d,%d)  to process %d (%d,%d)\n", myProcessID, cell.first, cell.second, to_processID, i, cell.second);
                 MPI_Send(&message, 2, MPI_INT, to_processID, TAG, MPI_COMM_WORLD);
             }
         } else {
             int message[2];
             message[0] = cell.first;
             message[1] = cell.second;
-            for (int i = cell.second + 1; i < N - 1; i++) {
+            int mid = 0;
+            for (int i = cell.second + 1; i < N; i++) {
                 //send to (cell.first, i)
                 int to_processID = cell2process[make_pair(cell.first, i)];
-                printf("process %d sending msg to %d, content (%d,%d)\n", myProcessID, to_processID, cell.first, cell.second);
+                printf("process %d sending (%d,%d)  to process %d (%d,%d)\n", myProcessID, cell.first, cell.second, to_processID, cell.first, i);
                 MPI_Send(&message, 2, MPI_INT, to_processID, TAG, MPI_COMM_WORLD);
+                //string msg;
+                //msg = to_string(cell.first) + "," + to_string(i), " msgid:" + to_string(mid);
+                //sendMessage(msg, to_processID, TAG);
             }
             for (int i = cell.first - 1; i >= 0; i--) {
                 // send to (i, cell.second)
                 int to_processID = cell2process[make_pair(i, cell.second)];
-                printf("process %d sending msg to %d, content (%d,%d)\n", myProcessID, to_processID, cell.first, cell.second);
+                printf("process %d sending (%d,%d)  to process %d (%d,%d)\n", myProcessID, cell.first, cell.second, to_processID, i, cell.second);
                 MPI_Send(&message, 2, MPI_INT, to_processID, TAG, MPI_COMM_WORLD);
-
             }
         }
 
