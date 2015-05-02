94d20551075d19a4929b257f618d52a585ebb95f
ankit <ankitgarg2008@gmail.com>
2015-05-02 19:19:21 -0400

    sending and receiving string messages instead of hard-coded cell indices

--- src/mylife.cpp
+++ src/mylife.cpp
@@ -1,8 +1,7 @@
 #include <mpi.h>
-#include <stdio.h>
 #include <vector>
 #include "port.h"
-#include <utility>
+#include "Grammar.h"
 
 using namespace std;
 
@@ -22,9 +21,13 @@ int tagHash(int i, int j);
 
 string receiveMessage(int source, int tag);
 
+void split(vector<string> &tokens, const string &text, char sep);
+
 int main(int argc, char **argv) {
     // Initialize the MPI environment
 
+    Grammar *g = new Grammar("");
+    string sentence[] = {"a", "b", "c", "d"};
 
     MPI_Init(NULL, NULL);
     int myProcessID;
@@ -62,92 +65,77 @@ int main(int argc, char **argv) {
 
     for (Cell cell : stack) {
         printf("%d, cell: %d,%d\n", myProcessID, cell.first, cell.second);
-        if (cell.first == cell.second) {
 
-        } else {
-            vector<Cell> recievedCells;
+        unordered_map<Cell, vector<string>, CellHash> receivedMessages;
+        if (cell.first != cell.second) {
             for (int j = cell.first; j < cell.second; j++) { // receive from previous row cells
-                //receive form (cell.first, j)
-                int recv[2] = {-1};
                 int from_processID = cell2process[make_pair(cell.first, j)];
                 int tag = tagHash(cell.first, j);
-//                printf("process %d cell (%d,%d) trying from process %d cell (%d,%d), tag %d\n", myProcessID, cell.first,
-//                       cell.second, from_processID, cell.first, j, tag);
-                MPI_Recv(recv, 2, MPI_INT, from_processID, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-                Cell reCell = make_pair(recv[0], recv[1]);
-//                printf("process %d cell (%d,%d) received from process %d cell (%d,%d)\n", myProcessID, cell.first,
-//                       cell.second, from_processID, recv[0], recv[1]);
-                recievedCells.push_back(reCell);
+                const string &basic_string = receiveMessage(from_processID, tag);
+                vector<string> rawData;
+                split(rawData, basic_string, ',');
+                Cell reCell = make_pair(stoi(rawData[0]), stoi(rawData[1]));
+                rawData.erase(rawData.begin() + 0);
+                rawData.erase(rawData.begin() + 1);
+                receivedMessages[reCell] = rawData;
             }
             for (int i = cell.second; i > cell.first; i--) {
-                //reveice from (i, cell.second)
-                int recv[2] = {-1};
                 int from_processID = cell2process[make_pair(i, cell.second)];
                 int tag = tagHash(i, cell.second);
-//                printf("process %d cell (%d,%d) trying from process %d cell (%d,%d), tag %d\n", myProcessID, cell.first,
-//                       cell.second, from_processID, i, cell.second, tag);
-                MPI_Recv(recv, 2, MPI_INT, from_processID, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-                Cell reCell = make_pair(recv[0], recv[1]);
-//                printf("process %d cell (%d,%d) received from process %d cell (%d,%d)\n", myProcessID, cell.first,
-//                       cell.second, from_processID, recv[0],
-//                       recv[1]);
-                recievedCells.push_back(reCell);
-            }
-            for (Cell reCell : recievedCells) {
-                printf("process %d cell (%d,%d) , received  content (%d,%d)\n", myProcessID, cell.first, cell.second,
-                       reCell.first, reCell.second);
+                const string &basic_string = receiveMessage(from_processID, tag);
+                vector<string> rawData;
+                split(rawData, basic_string, ',');
+                Cell reCell = make_pair(stoi(rawData[0]), stoi(rawData[1]));
+                rawData.erase(rawData.begin() + 0);
+                rawData.erase(rawData.begin() + 1);
+                receivedMessages[reCell] = rawData;
             }
         }
 
+        string message = to_string(cell.first) + "," + to_string(cell.second) + ",";
         // now compute the sends
         if (cell.first == cell.second) {
             // no wait  need to start sending
-            int message[2];
-            message[0] = cell.first;
-            message[1] = cell.second;
-            for (int i = cell.second + 1; i < N; i++) {
-                //send to (cell.first, i)
-                int to_processID = cell2process[make_pair(cell.first, i)];
-                int tag = tagHash(cell.first, cell.second);
-//                printf("process %d sending (%d,%d)  to process %d (%d,%d), tag %d\n", myProcessID, cell.first,
-//                       cell.second, to_processID, cell.first, i, tag);
-                MPI_Send(&message, 2, MPI_INT, to_processID, tag, MPI_COMM_WORLD);
-                //string msg;
-                //msg = to_string(cell.first) + "," + to_string(i), " msgid:" + to_string(mid);
-                //sendMessage(msg, to_processID, TAG);
-            }
-            for (int i = cell.first - 1; i >= 0; i--) {
-                // send to (i, cell.second)
-                int to_processID = cell2process[make_pair(i, cell.second)];
-                int tag = tagHash(cell.first, cell.second);
-//                printf("process %d sending (%d,%d)  to process %d (%d,%d), tag %d\n", myProcessID, cell.first,
-//                       cell.second, to_processID, i, cell.second, tag);
-                MPI_Send(&message, 2, MPI_INT, to_processID, tag, MPI_COMM_WORLD);
+            string terminal = sentence[cell.first];
+            const vector<string> &vector = g->getLHS(terminal);
+            for (string s : vector) {
+                message += s;
+                message += ",";
             }
         } else {
-            int message[2];
-            message[0] = cell.first;
-            message[1] = cell.second;
-            for (int i = cell.second + 1; i < N; i++) {
-                //send to (cell.first, i)
-                int to_processID = cell2process[make_pair(cell.first, i)];
-                int tag = tagHash(cell.first, cell.second);
-//                printf("process %d sending (%d,%d)  to process %d (%d,%d), tag %d\n", myProcessID, cell.first,
-//                       cell.second, to_processID, cell.first, i, tag);
-                MPI_Send(&message, 2, MPI_INT, to_processID, tag, MPI_COMM_WORLD);
-                //string msg;
-                //msg = to_string(cell.first) + "," + to_string(i), " msgid:" + to_string(mid);
-                //sendMessage(msg, to_processID, TAG);
+            set<string> setString;
+
+            for (int l = cell.first; l <= cell.second; l++) {
+                Cell c1 = make_pair(cell.first, l);
+                Cell c2 = make_pair(l + 1, cell.second);
+                vector<string> first = receivedMessages[c1];
+                vector<string> second = receivedMessages[c2];
+                for (string s1 : first) {
+                    for (string s2 : second) {
+                        const vector<string> &vector = g->getLHS(s1, s2);
+                        for (string nonTerminal : vector) {
+                            setString.insert(nonTerminal);
+                        }
+                    }
+                }
             }
-            for (int i = cell.first - 1; i >= 0; i--) {
-                // send to (i, cell.second)
-                int to_processID = cell2process[make_pair(i, cell.second)];
-                int tag = tagHash(cell.first, cell.second);
-//                printf("process %d sending (%d,%d)  to process %d (%d,%d), tag %d\n", myProcessID, cell.first,
-//                       cell.second, to_processID, i, cell.second, tag);
-                MPI_Send(&message, 2, MPI_INT, to_processID, tag, MPI_COMM_WORLD);
+
+            for (string nonTerminal : setString) {
+                message += nonTerminal;
+                message += ",";
             }
         }
+        for (int i = cell.second + 1; i < N; i++) {
+            int to_processID = cell2process[make_pair(cell.first, i)];
+            int tag = tagHash(cell.first, cell.second);
+            sendMessage(message, to_processID, tag);
+        }
+        for (int i = cell.first - 1; i >= 0; i--) {
+            int to_processID = cell2process[make_pair(i, cell.second)];
+            int tag = tagHash(cell.first, cell.second);
+            sendMessage(message, to_processID, tag);
+        }
+
     }
     MPI_Finalize();
 
@@ -175,3 +163,12 @@ void sendMessage(const string &bla, int dest, int tag) {
     MPI::COMM_WORLD.Send(bla.c_str(), bla.length(), MPI::CHAR, dest, tag);
 }
 
+void split(vector<string> &tokens, const string &text, char sep) {
+    int start = 0, end = 0;
+
+    while ((end = text.find(sep, start)) != string::npos) {
+        tokens.push_back(text.substr(start, end - start));
+        start = end + 1;
+    }
+    tokens.push_back(text.substr(start));
+}
